1ad6a997ad0b9f826f9a74da9bba34bc57bcafae
NavyaTrilok <dtriloknavya@gmail.com>
2022-05-01 12:25:01 +0530

    Time: 52 ms (33.24%), Space: 49.3 MB (56.50%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -38,19 +38,48 @@ class Solution {
 
         return memo[idx]=ans+1;
     }
+    
+    public static int tab(int n,int[] arr){
+        
+        
+        int[] dp=new int[n];
+
+        //if(memo[idx]!=-1) return memo[idx];
+        
+        for(int idx=n-1;idx>=0;idx--){
+            
+             if(idx==n-1){
+                 dp[idx]=0;
+              continue;
+        }
+            
+             int jumps_allowed=arr[idx];
+
+            int ans=(int)(1e8);
+
+            for(int j=1;j<=jumps_allowed;j++){
+                if(idx+j<=n-1){
+                    ans=Math.min(ans,dp[idx+j]);
+                }
+            }
+            dp[idx]=ans+1;
+        }
+        
+        return dp[0];
+    }
 
     
     public int jump(int[] nums) {
         int n=nums.length;
-        int[] memo=new int[n];
+       // int[] memo=new int[n];
         
-        Arrays.fill(memo,-1);
+        //Arrays.fill(memo,-1);
         
         //int ans= rec(n,0,nums);
-        int ans= rec_memo(n,0,nums,memo);
+        //int ans= rec_memo(n,0,nums,memo);
+        int ans= tab(n,nums);
         
         return ans;
-      
-        
+         
     }
 }
