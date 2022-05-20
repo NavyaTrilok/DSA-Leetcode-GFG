24db8c25ed67a7f34f62dedbd252c7fb2a76522e
NavyaTrilok <dtriloknavya@gmail.com>
2022-05-20 15:44:55 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (92.26%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int rob(int[] nums) {
+   /* public int rob(int[] nums) {
         int n=nums.length;
         
         int[] inc=new int[n];
@@ -14,6 +14,25 @@ class Solution {
         }
         
         return Math.max(inc[n-1],exc[n-1]);      
+        
+    }*/
+    
+     public int rob(int[] nums) {
+        int n=nums.length;
+           
+        
+        int inc=nums[0];
+        int exc=0;
+        
+        for(int i=1;i<n;i++){
+            int ninc=exc+nums[i];
+            int nexc=Math.max(exc,inc);
+            
+            inc=ninc;
+            exc=nexc;
+        }
+        
+        return Math.max(inc,exc);      
         
     }
 }
