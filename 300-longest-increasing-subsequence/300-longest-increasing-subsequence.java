9c9a07890e49b5da82e7e6cfaec7f8ca6fbc5e5f
NavyaTrilok <dtriloknavya@gmail.com>
2022-05-29 08:55:35 +0530

    Time: 6 ms (82.56%), Space: 44.7 MB (33.66%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
+   /* public int lengthOfLIS(int[] nums) {
         int n=nums.length;
         
         int[] dp= new int[n];
@@ -18,8 +18,48 @@ class Solution {
             lis=Math.max(lis,dp[i]);
         }
         
-        return lis;
+        return lis;       
+    }*/
+    
+    //Longest Increasing Subsequence in NlogN
+    
+    public int BS(ArrayList<Integer> dp, int ele){
+        int si=0;
+        int ei=dp.size()-1;
         
         
+        while(si<=ei){
+            int mid=(si+ei)/2;
+            
+            if(dp.get(mid)<ele){
+                si=mid+1;
+            }else{
+                ei=mid-1;
+            }
+        }
+        
+        return si;
     }
+    
+    
+    public int lengthOfLIS(int[] nums){
+        int n=nums.length;
+        
+        ArrayList<Integer> dp=new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            int ele=nums[i];
+            
+            int pos=BS(dp,ele);
+            
+            if(pos==dp.size()){
+                dp.add(ele);
+            }else{
+                dp.set(pos,ele);
+            }
+        }
+        
+        return dp.size();
+    }
+    
 }
