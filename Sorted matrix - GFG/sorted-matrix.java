59140a114bc4e6d192812f9244a687ec2f50e60e
NavyaTrilok <dtriloknavya@gmail.com>
2022-06-25 19:54:22 +0530

    Added solution - LeetHub

---
+++ Sorted matrix - GFG/sorted-matrix.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            int v[][] = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                String s[] = read.readLine().split(" ");
+                for (int j = 0; j < N; j++) v[i][j] = Integer.parseInt(s[j]);
+            }
+            Solution ob = new Solution();
+            int ans[][] = ob.sortedMatrix(N, v);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) System.out.print(ans[i][j] + " ");
+                System.out.println();
+            }
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int[][] sortedMatrix(int N, int Mat[][]) {
+           List<Integer> temp = new ArrayList<Integer>();
+ 
+    // Store all elements of matrix into temp
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        temp.add(Mat[i][j]);
+      }
+    }
+ 
+    // Sort the temp
+    Collections.sort(temp);
+ 
+    // Print the values of temp
+    int count=0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++){
+      Mat[i][j]=temp.get(count);
+      count++;
+    }
+    }
+    return Mat;
+    }
+}
