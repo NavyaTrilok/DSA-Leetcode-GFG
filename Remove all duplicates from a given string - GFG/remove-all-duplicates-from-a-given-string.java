51486c130561e10088b03c190e52752d9c820e07
NavyaTrilok <dtriloknavya@gmail.com>
2022-06-27 15:35:56 +0530

    Added solution - LeetHub

---
+++ Remove all duplicates from a given string - GFG/remove-all-duplicates-from-a-given-string.java
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String str = br.readLine().trim();
+
+            String ans = new Solution().removeDuplicates(str);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    String removeDuplicates(String str) {
+        
+        String nstr="";
+        
+       LinkedHashSet<Character> lhs = new LinkedHashSet<>();
+        for(int i=0;i<str.length();i++)
+            lhs.add(str.charAt(i));
+         
+        // print string after deleting duplicate elements
+        for(Character ch : lhs)
+            nstr=nstr+ch;
+            
+            
+        return nstr;
+            
+    }
+}
+
