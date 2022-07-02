1ac4e9fdcc5af023f4f151444d996c9c5d43e011
NavyaTrilok <dtriloknavya@gmail.com>
2022-07-02 21:51:47 +0530

    Time: 12 ms (20.81%), Space: 45.1 MB (41.76%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,17 +1,12 @@
 class Solution {
-    public int minDistance(String str1, String str2) {
+    public int minDistance(String word1, String word2) {
         
-        /*if(word1=="" && word2=="") 
-            return 0;
-        else if(word1=="") 
-            return word2.length();
-        else if(word2=="")
-            return word1.length();
+       
         int m=word1.length();
         int n=word2.length();
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
                 if(i==0) dp[i][j]=j;
                 else if(j==0) dp[i][j]=i;
                 else if(word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
@@ -22,10 +17,10 @@ class Solution {
             }
         }
         
-        return dp[m-1][n-1];*/
+        return dp[m][n];
         
         
-        int m = str1.length();
+       /* int m = str1.length();
           int n = str2.length();
           
           int[][] dp = new int[m+1][n+1];
@@ -42,6 +37,6 @@ class Solution {
               }
           }
           
-          return dp[m][n];
+          return dp[m][n];*/
     }
 }
