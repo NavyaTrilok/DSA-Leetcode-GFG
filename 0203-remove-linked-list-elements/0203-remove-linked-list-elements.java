99d8b5ca39dfe578cecc76fdb882a91837d474cd
NavyaTrilok <dtriloknavya@gmail.com>
2023-03-27 16:54:01 -0700

    Time: 0 ms (100.00%), Space: 43.8 MB (19.42%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -8,16 +8,23 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
-/*When the input node is an empty node, then there is nothing to delete, so we just return a null node back. (That's the first line)
-
-When the head of the input node is the target we want to delete, we just return head.next instead of head to skip it. (That's the third line), else we will return head.
-
-We apply the same thing to every other node until it reaches null. (That's the second line).*/
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if(head == null) return null;
-        head.next = removeElements(head.next,val);
-        return head.val == val? head.next : head;
+        if(head == null)
+            return null;
+        
+        
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode curr = dummy;
+        
+        while(curr.next != null){
+            if(curr.next.val == val){
+                curr.next = curr.next.next;
+            }else{
+                curr = curr.next;
+            }
+        }
+        return dummy.next;
     }
 }
