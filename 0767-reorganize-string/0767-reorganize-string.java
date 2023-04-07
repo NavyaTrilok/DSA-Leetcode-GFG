6845e29d819d524f87710452b4e68653b45c35af
NavyaTrilok <dtriloknavya@gmail.com>
2023-04-07 12:28:26 -0700

    Time: 3 ms (65.14%), Space: 40.5 MB (70.00%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -2,20 +2,20 @@ class Solution {
     public String reorganizeString(String s) {
         Map<Character, Integer> bucket1 = new HashMap<>();
         int maxCount = 0;
-        for(char curChar : s.toCharArray()){
-            bucket1.put(curChar,bucket1.getOrDefault(curChar,0) + 1);
-            maxCount = Math.max(maxCount, bucket1.get(curChar));
+        for(char currentChar : s.toCharArray()){
+            bucket1.put(currentChar,bucket1.getOrDefault(currentChar,0) + 1);
+            maxCount = Math.max(maxCount, bucket1.get(currentChar));
         }
         
         if(maxCount > s.length() / 2 + 1)
             return "";
         Map<Integer, List<Character>> bucket2 = new HashMap<>();
-        for(Character curChar : bucket1.keySet()){
-            int freq = bucket1.get(curChar);
+        for(Character currentChar : bucket1.keySet()){
+            int freq = bucket1.get(currentChar);
             if(!bucket2.containsKey(freq)){
                 bucket2.put(freq,new ArrayList<>());
             }
-            bucket2.get(freq).add(curChar);
+            bucket2.get(freq).add(currentChar);
         }
         char[] res = new char[s.length()];
         int index = 0;
@@ -23,11 +23,11 @@ class Solution {
             if(!bucket2.containsKey(count))
                 continue;
             List<Character> list = bucket2.get(count);
-            for(Character curChar: list){
-                for(int curCount = 0; curCount < count; curCount++){
-                    if(index == 1 && curChar == res[index - 1])
+            for(Character currentChar: list){
+                for(int currentCount = 0; currentCount < count; currentCount++){
+                    if(index == 1 && currentChar == res[index - 1])
                         return "";
-                    res[index] = curChar;
+                    res[index] = currentChar;
                     index += 2;
                     if(s.length() <= index){
                         index = 1;
