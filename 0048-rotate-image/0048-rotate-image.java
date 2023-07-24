443f5d9a9085601999cd32a115ef6cc3a3b4ac95
NavyaTrilok <dtriloknavya@gmail.com>
2023-07-24 15:16:40 -0700

    Time: 0 ms (100.00%), Space: 41.4 MB (40.30%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,40 +1,44 @@
 class Solution {
     public void rotate(int[][] matrix) {
         transpose(matrix);
-        
     }
     
     public void transpose(int[][] matrix){
         int n = matrix.length;
-        //int diag = 1;
-        
-        for(int diag = 1; diag < n; diag++){
+        for(int diag = 1; diag <= n; diag++){
             int row = 0, col = diag;
             while(col<n){
                 int temp = matrix[row][col];
                 matrix[row][col] = matrix[col][row];
                 matrix[col][row] = temp;
                 
+                
                 row++;
                 col++;
             }
+            //row++;
         }
-        reverserows(matrix);
+        
+        reverserow(matrix);
     }
     
-    public void reverserows(int[][] matrix){
+    public void reverserow(int[][] matrix){
         int n = matrix.length;
-        for(int row = 0; row<n; row++){
+        for(int row = 0 ; row < n ; row++){
             int i = 0;
-            int j = n - 1;
+            int j = n-1;
             while(i<=j){
                 int temp = matrix[row][i];
                 matrix[row][i] = matrix[row][j];
                 matrix[row][j] = temp;
                 
+                
                 i++;
                 j--;
             }
+            
+            
+                
         }
     }
 }
